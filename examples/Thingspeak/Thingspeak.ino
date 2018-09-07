ad626a47eb74c5d5b0764194f4c5031874d1e37d
Metin KOC <saucompeng@gmail.com>
2018-09-07 17:38:38 +0300

    ThingSpeak example added.

---
+++ examples/Thingspeak/Thingspeak.ino
@@ -0,0 +1,44 @@
+/*
+  localHost.ino - This is basic local host example.
+  Created by Yasin Kaya (selengalp), August 18, 2018.
+*/
+
+#include "Sixfab_CellularIoT.h"
+
+SixfabCellularIoT node;
+
+char api_key[] = "3EHEKVIHWCZELJF7"; // change with api-key
+
+char dataTemplate[] ="field1=%d";
+
+char dataJSON[100];
+
+// setup
+void setup() {
+  
+  node.init();
+
+  node.getIMEI();
+  node.getFirmwareInfo();
+  node.getHardwareInfo();
+
+  node.setGSMBand(GSM_900);
+  node.getBandConfiguration();  
+  node.setMode(GSM_MODE);
+  
+  node.connectToOperator();
+  node.getSignalQuality();
+  node.getQueryNetworkInfo();
+
+  node.deactivateContext();
+  node.activateContext();
+
+  sprintf(dataJSON, dataTemplate, 22);
+  node.sendDataThingspeak(api_key, dataJSON );
+  
+}
+
+// loop
+void loop() {
+
+}
